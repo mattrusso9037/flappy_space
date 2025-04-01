4be95587e207f8bff6e865c53d0d5707d5df58a2
Matt Russo <mattrusso9037@gmail.com>
2025-04-01 11:27:03 -0400

    Deploy to GitHub Pages

--- electron/preload.cjs
+++
@@ -1,7 +0,0 @@
-const { contextBridge } = require('electron');
-
-// Expose a limited API to the renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-  // You can add functions here that will be accessible from the renderer process
-  // Example: getAppVersion: () => app.getVersion(),
-}); 
--- src/game/entities/index.ts
+++
@@ -1,6 +0,0 @@
-export { Astronaut } from './Astronaut';
-export { Obstacle, PipeObstacle } from './Obstacle';
-export { Planet } from './Planet';
-export { Star } from './Star';
-export { rectanglesIntersect } from './utils';
-export { Orb } from './Orb'; 
